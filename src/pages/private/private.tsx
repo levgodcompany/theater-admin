073d33354b83c145d68c4c355cfe0ec9e416ae22
leandroveroninf <levgodcompany@gmail.com>
2024-07-08 02:37:59 -0300

    vercel

--- src/pages/private/private.tsx
+++ src/pages/private/private.tsx
@@ -6,10 +6,8 @@ import { PrivateRoutes } from "../../routes/routes";
 
 const Home = lazy(() => import("./Home/Home"));
 const Local = lazy(() => import("./Local/Local"));
-const Clients = lazy(() => import("./Clients/Clients"));
 const Rooms = lazy(() => import("./Rooms/Rooms"));
 const Room = lazy(() => import("./Room/Room"));
-const Shifts = lazy(() => import("./Clients/Shifts/Shifts"));
 
 function Private() {
   return (
@@ -17,10 +15,8 @@ function Private() {
       <Route index element={<Navigate to={`${PrivateRoutes.HOME}`} />} />
       <Route path={`${PrivateRoutes.HOME}`} element={<Home />} />
       <Route path={`${PrivateRoutes.LOCAL}`} element={<Local />} />
-      <Route path={`${PrivateRoutes.CLIENTS}`} element={<Clients />} />
       <Route path={`${PrivateRoutes.ROOMS}`} element={<Rooms />} />
       <Route path={`${PrivateRoutes.ROOM}/*`} element={<Room />} />
-      <Route path={`${PrivateRoutes.SHIFTS}`} element={<Shifts />} />
     </RoutesWithNotFound>
   );
 }
